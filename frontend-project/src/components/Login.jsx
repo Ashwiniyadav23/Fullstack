6a2055cad26332d4df4f2f0678bbbd458f68bff0
Ashwini Yadav <ashwiniyadav23@navgurukul.org>
2025-02-07 09:56:03 +0530

    Update Login.jsx

--- frontend-project/src/components/Login.jsx
+++ frontend-project/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5002/api/getAll");
+      const response = await fetch("https://fullstack-topaz.vercel.app/api/getAll");
       const users = await response.json();
 
       const userExists = users.find(
